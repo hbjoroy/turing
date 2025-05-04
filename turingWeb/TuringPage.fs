354a7d1be1f6ff0a3e9774015d22bca04e2a3242
Harald Bjorøy <harald@kven.no>
2025-05-04 19:09:49 +0200

    Starting with the UI - copilot style...

--- turingWeb/TuringPage.fs
+++ turingWeb/TuringPage.fs
@@ -1,31 +1,7 @@
 module TuringPage
+open System.IO
 
-let template = """
-<!DOCTYPE html>
-<html lang="no">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Turing Machine</title>
-    <link rel="stylesheet" href="/css/turing.css">
-</head>
-<body>
-    <script src="/js/scripts.js"></script>
-    <section id="turingRoot">
-        <div id="turingMachine">
-            <div id="tape"></div>
-            <div id="controls">
-                <button id="startButton">Start</button>
-                <button id="stopButton">Stop</button>
-                <button id="resetButton">Reset</button>
-            </div>
-            <div id="state"></div>
-            <input type="textarea" id="turingInstructions" placeholder="Enter input here">
-        </div>
-    </section>
-</body>
-</html>
-"""
 let turingPage (context: Microsoft.AspNetCore.Http.HttpContext) =
+    let template = File.ReadAllText "TuringPageTemplate.html"
     context.Response.ContentType <- "text/html"
     template
