fba8737e37dd99d73e326b323c887d551270a372
Harald Bjorøy <harald@kven.no>
2025-05-07 06:36:22 +0200

    Lagt til vue https og proxy

--- turingWeb/Program.fs
+++ turingWeb/Program.fs
@@ -2,7 +2,6 @@ open System
 open Microsoft.AspNetCore.Builder
 open Microsoft.Extensions.Hosting
 open Microsoft.Extensions.DependencyInjection
-open TuringPage
 open TuringService
 open Microsoft.AspNetCore.Http
 open System.Threading.Tasks
@@ -19,9 +18,7 @@ let main args =
     
     let app = builder.Build()
     app.UseSession() |> ignore
-    app.UseStaticFiles(StaticFileOptions()) |> ignore
-    // app.UseWebSockets() |> ignore
-    app.MapGet("/", Func<HttpContext,string>(turingPage)) |> ignore
+    app.UseStaticFiles() |> ignore    
     app.MapPost("/service/turing", Func<TuringInput,Task<IResult>>
         (fun message -> createTuringMachine message.instructions)) |> ignore
     app.Run()
--- turingWeb/TuringPage.fs
+++
@@ -1,7 +0,0 @@
-module TuringPage
-open System.IO
-
-let turingPage (context: Microsoft.AspNetCore.Http.HttpContext) =
-    let template = File.ReadAllText "TuringPageTemplate.html"
-    context.Response.ContentType <- "text/html"
-    template
