b7ff120b73e0bde20a5a2db2902469598ce05226
Harald Bjorøy <harald@kven.no>
2026-03-22 20:24:12 +0100

    vets

--- turing-vue/src/main.ts
+++ turing-vue/src/main.ts
@@ -1,6 +1,7 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+// import App from './App.vue'
+// createApp(App).mount('#app')
+import TuringApp from './TuringApp.vue'
+createApp(TuringApp).mount('#app')
